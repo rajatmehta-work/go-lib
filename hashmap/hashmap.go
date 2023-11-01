9d2dc0a2585d6b57cf7398c54ec49570e8826117
rajat mehta <rajatmehta.work@gmail.com>
2023-11-01 16:20:22 +0530

    using ptr

--- hashmap/hashmap.go
+++ hashmap/hashmap.go
@@ -8,25 +8,25 @@ type Map[K comparable, V any] interface {
 }
 
 func New[K comparable, V any]() Map[K, V] {
-	return Map[K, V](store[K, V]{ptrRef: make(map[K]V)})
+	return &store[K, V]{ptrRef: make(map[K]V)}
 }
 
 type store[K comparable, V any] struct {
 	ptrRef map[K]V
 }
 
-func (p store[K, V]) Set(key K, val V) {
+func (p *store[K, V]) Set(key K, val V) {
 	p.ptrRef[key] = val
 }
-func (p store[K, V]) Delete(key K) {
+func (p *store[K, V]) Delete(key K) {
 	delete(p.ptrRef, key)
 }
 
-func (p store[K, V]) Get(key K) (V, bool) {
+func (p *store[K, V]) Get(key K) (V, bool) {
 	val, ok := p.ptrRef[key]
 	return val, ok
 }
 
-func (p store[K, V]) Len() int {
+func (p *store[K, V]) Len() int {
 	return len(p.ptrRef)
 }
